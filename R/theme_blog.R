c00e0545ae5b7deaa7b2f986a0e216c944cef92f
William Fondrie <fondriew@gmail.com>
2020-10-01 17:28:21 -0700

    Changed colors and fonts to match CV and presentations

--- R/theme_blog.R
+++ R/theme_blog.R
@@ -1,14 +1,14 @@
 
 library(ggplot2)
 
-theme_colors <- list("#494f5c", "#c6cddb", "#018574", "#7d828a")
+theme_colors <- list("#ffffff", "#404040", "#24B8A0", "#7d828a")
 names(theme_colors) <- c("bg", "fg", "hl", "accent")
 
-theme_pal <- c("#c6cddb", "#018574", "#f0b67f", "#fe5f55")
+theme_pal <- c("#404040", "#24B8A0", "#f0b67f", "#fe5f55")
 
-theme_blog <- function(base_size = 14) { 
-    theme_classic(base_size = base_size, base_family = "Tahoma") +
-    theme(plot.background = element_rect(fill = theme_colors$bg, color = NA),
+theme_blog <- function(base_size = 14) {
+    theme_classic(base_size = base_size, base_family = "Roboto") +
+    theme(plot.background = element_blank(),
           plot.margin = margin(20, 20, 10, 10, "pt"),
           panel.background = element_blank(),
           text = element_text(color = theme_colors$fg),
